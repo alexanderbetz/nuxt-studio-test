7988ddb690d5fc3e15d358c604272cb83ab9eb57
Alexander Betz <alexander.betz@deckweiss.at>
2023-07-11 08:56:49 +0200

    fix: nuxt studio token

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,11 @@
 export default defineNuxtConfig({
   extends: 'content-wind',
   runtimeConfig: {
-    public: {
-      studio: {
-        publicToken: 'feaa3ee3597ba06e401cdd565f91801f61c9b8a2f6a9ba721122fae857e97ae2'
-      }
+    studio: {
+      publicToken: 'feaa3ee3597ba06e401cdd565f91801f61c9b8a2f6a9ba721122fae857e97ae2'
     }
-  }
+  },
+  modules: [
+    '@nuxthq/studio'
+  ]
 })
