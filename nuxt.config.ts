2f3ce6a46af12c38a51fab23ef08b2e25afe8dc6
Alexander Betz <alexander.betz@deckweiss.at>
2023-07-11 08:15:26 +0200

    chore: added nuxt studio token

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,8 @@
 export default defineNuxtConfig({
-  extends: 'content-wind'
+  extends: 'content-wind',
+  config: {
+    public: {
+      NUXT_PUBLIC_STUDIO_TOKENS: "feaa3ee3597ba06e401cdd565f91801f61c9b8a2f6a9ba721122fae857e97ae2"
+    }
+  }
 })
