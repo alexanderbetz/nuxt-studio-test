91db073f7cd3e01230010a9a43b9ef64a16149cd
Alexander Betz <alexander.betz@deckweiss.at>
2023-07-13 21:26:28 +0200

    chore: added sveltekit portfolie site

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import '../app.css'
+</script>
+
+<div class="flex items-center flex-col justify-center min-h-full flex-1 dark:text-gray-200">
+  <slot></slot>
+</div>
+
+<style>
+  :global(html), :global(body), :global(.app-container) {
+    min-height: 100vh;
+  }
+
+  :global(.app-container) {
+    display: flex;
+  }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<div class="space-y-6">
+  <h1 class="text-3xl font-bold">Fullstack Webdevelopment</h1>
+
+  <div class="text-center">
+    <p>Alexander Betz</p>
+    <p>Schlossweg 20/2</p>
+    <p>3240 Mank</p>
+  </div>
+
+  <div class="text-center">
+    <p>Einzelunternehmen <span class="text-gray-400 dark:text-gray-500">soon</span></p>
+    <p>USt.-ID: <span class="text-gray-400 dark:text-gray-500">soon</span></p>
+  </div>
+
+  <div class="text-center">
+    <a href="mailto:office@alexanderbetz.com">office@alexanderbetz.com</a>
+  </div>
+</div>
